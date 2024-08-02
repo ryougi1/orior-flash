90db89be2486ca24d5e3e8fbbe9afbac72fb7466
ryougi1 <gulong.james@gmail.com>
2024-08-02 16:03:10 +0200

    #style: temp card styling

--- src/renderer/src/components/ImageCardComponent.tsx
+++ src/renderer/src/components/ImageCardComponent.tsx
@@ -1,4 +1,5 @@
 import { SessionCard } from '@renderer/models'
+import { useRef } from 'react'
 
 interface IProps {
   card: SessionCard
@@ -6,21 +7,42 @@ interface IProps {
 }
 
 function ImageCardComponent({ card, onCardFlip }: IProps): JSX.Element {
+  const frontRef = useRef<HTMLDivElement>(null)
+  const backRef = useRef<HTMLDivElement>(null)
+
   const onClick = (event): void => {
     onCardFlip()
+    handleFace()
     event.preventDefault()
+  }
+
+  const handleFace = (): void => {
+    if (frontRef.current && backRef.current) {
+      const frontImageElement = frontRef.current.querySelector('img')
+      if (!frontImageElement) {
+        return
+      }
+      if (card.isFlipped) {
+        backRef.current.style.width = `${frontImageElement.offsetWidth}px`
+        backRef.current.style.height = `${frontImageElement.offsetHeight}px`
+        frontRef.current.style.display = 'none'
+        backRef.current.style.display = 'flex'
+      } else {
+        frontRef.current.style.display = 'block'
+        backRef.current.style.display = 'none'
+      }
+    }
   }
 
   return (
     <>
       <div onClick={onClick} className="card">
-        {card.isFlipped ? (
-          <div className="back">
-            <div>{card.back} </div>
-          </div>
-        ) : (
-          <img src={card.front} width={400} height={400} />
-        )}
+        <div className="back" ref={backRef}>
+          <span>{card.back.toLocaleUpperCase()}</span>
+        </div>
+        <div className="front" ref={frontRef}>
+          <img src={card.front} />
+        </div>
       </div>
     </>
   )
